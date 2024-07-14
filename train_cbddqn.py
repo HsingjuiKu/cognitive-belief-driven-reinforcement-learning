39312f205105d6f9bd5148be14a1dbe6210c65e2
HsingjuiKu <xingrui.gu@outlook.com>
2024-07-14 15:06:59 +0800

    Update train_cbddqn.py

--- train_cbddqn.py
+++ train_cbddqn.py
@@ -18,7 +18,7 @@ def parse_args():
     parser.add_argument("--env", type=str, default="atari")
     parser.add_argument("--env-id", type=str, default="Breakout-v5")
     parser.add_argument("--test", type=int, default=0)
-    parser.add_argument("--device", type=str, default="mps:0")
+    parser.add_argument("--device", type=str, default="cuda:0")
 
     return parser.parse_args()
 
