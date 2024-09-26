6dc4bb5cdc61e61f3ddc50ae905736b9fd037937
Xingrui Gu/Hsingjui Ku/Hayden Gu <87567247+HsingjuiKu@users.noreply.github.com>
2024-09-27 04:35:05 +0800

    update my laptop

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ from xuance import get_runner
 
 def parse_args():
     parser = argparse.ArgumentParser("Run a demo.")
-    parser.add_argument("--method", type=str, default="dqn")
-    parser.add_argument("--env", type=str, default="atari")
-    parser.add_argument("--env-id", type=str, default="ALE/Breakout-v5")
+    parser.add_argument("--method", type=str, default="cbddqn")
+    parser.add_argument("--env", type=str, default="metadrive")
+    parser.add_argument("--env-id", type=str, default="metadrive")
     parser.add_argument("--test", type=int, default=0)
     parser.add_argument("--device", type=str, default="cuda:0")
 
